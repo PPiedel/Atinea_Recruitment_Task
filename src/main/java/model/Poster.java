3376b60f16d2932b5664741fc10328a249a92462
PPiedel <pawelpiedel@yahoo.pl>
2018-04-16 18:30:05 +0200

    Model class created.

---
+++ src/main/java/model/Poster.java
@@ -0,0 +1,19 @@
+package model;
+
+public class Poster {
+    private int height;
+    private int width;
+
+    public Poster(int height, int width) {
+        this.height = height;
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+}
